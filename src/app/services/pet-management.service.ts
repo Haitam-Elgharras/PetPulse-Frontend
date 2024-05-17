7d441774c6ddb7e98a69ec0b179d4ea43f8a4a0e
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-05-17 17:32:14 +0100

    Added Pet Management Service

---
+++ src/app/services/pet-management.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+import {Pet} from "../models/pet.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetManagementService {
+
+  constructor(private http : HttpClient) { }
+
+  baseUrl = environment.baseUrl;
+
+  getPetsByUserId(userId: number, page: number, size: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/pets/user/${userId}?page=${page}&size=${size}`);
+  }
+
+  public createPet(petData: FormData): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/pets/save`, petData);
+  }
+
+  public getPetById(petId: number): Observable<Pet> {
+    return this.http.get<Pet>(`${this.baseUrl}/pets/${petId}`);
+  }
+
+}
